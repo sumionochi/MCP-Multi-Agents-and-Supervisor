b3df4424454d72f2d1e5a06aa98b9da01d84913b
sumionochi <aaditya.srivastava.connect@gmail.com>
2026-09-04 12:00:06 +0200

    Connected latest Bright Data MCP server while removing the web_unlocker as its been made paid but the code will still work

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import os
 import asyncio
 from dotenv import load_dotenv
 
-load_dotenv()    
+from langchain_mcp_adapters.client import MultiServerMCPClient
 
-if _name_ == "main":
+load_dotenv()
+
+
+async def run_agent1():
+    client = MultiServerMCPClient(
+        {
+            "Bright Data": {
+                "command": "npx",
+                "args": ["@brightdata/mcp"],
+                "env": {
+                    "API_TOKEN": os.getenv("BRIGHT_DATA_API_TOKEN"),
+                },
+                "transport": "stdio",
+            },
+        }
+    )
+
+    tools = await client.get_tools()
+
+
+if __name__ == "__main__":
     asyncio.run(run_agent1())
